dc1704079ab14dc0098ed0d7e82af7300e6846d3
Montso-Matlaletsa <montso.matlaletsa@gmail.com>
2022-01-19 08:02:19 -0800

    Adds ui to diary app

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+const filterStories = ()=>{
+
+    let filter = document.querySelector('#mySearch').value;
+    let stories = document.querySelectorAll('#myMenu li');
+
+    let search = filter.toLowerCase();
+
+    for (let i of stories) {
+        let item = i.innerHTML.toLowerCase();
+       
+      if (item.indexOf(search) == -1) {i.classList.add("hide"); } //add hide class to an item
+      else {i.classList.remove("hide");} //remove hide class
+    }
+}
